5fe21b58d033857d8ddbd3e68e84347ac54c7e6b
discoveranirban <discoveranirban@gmail.com>
2024-04-02 23:45:43 +0530

    join game code pt1

--- websockets-demo/index.js
+++ websockets-demo/index.js
@@ -21,7 +21,7 @@ wsServer.on("request", request => {
     connection.on("close", () => console.log("closed"));
     connection.on("message", message => {
         const result = JSON.parse(message.utf8Data);
-         //I have received a message from the client
+        //I have received a message from the client
         //a user want to create a new game
         if (result.method === "create") {
             const clientId = result.clientId;
@@ -39,6 +39,33 @@ wsServer.on("request", request => {
 
             const con = clients[clientId].connection;
             con.send(JSON.stringify(payLoad));
+        }
+
+        //a client want to join
+        if (result.method === "join") {
+
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];
+
+            if (game.clients.length >= 3) return;
+            
+            const color =  {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length];
+
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            });
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            };
+
+            //loop through all clients and tell them that people has joined
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad))
+            });
         }
 
     });
